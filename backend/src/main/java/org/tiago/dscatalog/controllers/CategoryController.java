69d9c74367f85df6c32c7b0976209b63ac25a77a
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-02-25 14:50:27 -0300

    feat: add Category controller

---
+++ backend/src/main/java/org/tiago/dscatalog/controllers/CategoryController.java
@@ -0,0 +1,10 @@
+package org.tiago.dscatalog.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+}
