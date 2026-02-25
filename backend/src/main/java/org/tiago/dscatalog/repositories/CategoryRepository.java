262f4f9ac089a7f0c4cf27dd31a28396010acb65
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-02-25 15:09:17 -0300

    feat: add Category repository

---
+++ backend/src/main/java/org/tiago/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package org.tiago.dscatalog.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.tiago.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
