6fa4fdae3761ba6c09149e4554bdce88f939ff1a
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-02-25 15:39:23 -0300

    feat(category): add findAll endpoint

--- backend/src/main/java/org/tiago/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/org/tiago/dscatalog/controllers/CategoryController.java
@@ -1,10 +1,25 @@
 package org.tiago.dscatalog.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.tiago.dscatalog.entities.Category;
+import org.tiago.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
-
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> listOfAllElements = service.findAll(); 
+		return ResponseEntity.ok().body(listOfAllElements);
+	}
 }
