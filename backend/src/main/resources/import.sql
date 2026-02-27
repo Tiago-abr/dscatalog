4207f795ecf7298143063dedcaea72d30f4cf2f5
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-02-26 21:28:25 -0300

    chore(seeding): add dummy data

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_category(name) VALUES('Books');
+INSERT INTO tb_category(name) VALUES('Electronics');
+INSERT INTO tb_category(name) VALUES('Games');
+INSERT INTO tb_category(name) VALUES('Computers');
+INSERT INTO tb_category(name) VALUES('Sports & Outdoors');
+INSERT INTO tb_category(name) VALUES('Cell Phones & Accessories');
+
