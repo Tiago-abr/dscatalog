e74af31a41540738b990d6fe080adc7d2d865558
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-03-09 16:47:31 -0300

    feat(CategoryDTO): add constructor

--- backend/src/main/java/org/tiago/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/org/tiago/dscatalog/dto/CategoryDTO.java
@@ -1,3 +1,10 @@
 package org.tiago.dscatalog.dto;
 
-public record CategoryDTO(Long id, String name) {}
+import org.tiago.dscatalog.entities.Category;
+
+public record CategoryDTO(Long id, String name) {
+	
+	public CategoryDTO(Category category) {
+		this(category.getId(), category.getName());
+	}
+}
