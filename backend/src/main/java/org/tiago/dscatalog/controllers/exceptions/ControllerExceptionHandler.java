0563de0dc40f0a432f9e2e6d1aa54bcd189d8ada
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-03-11 19:57:48 -0300

    feat: add ControllerExceptionHandler class and entityNotFound method

---
+++ backend/src/main/java/org/tiago/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.tiago.dscatalog.controllers.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.tiago.dscatalog.exceptions.EntityNotFoundException;
+import org.tiago.dscatalog.exceptions.StandardError;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException exception, HttpServletRequest request){
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setError("Resource not found");
+		error.setMessage(exception.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}
