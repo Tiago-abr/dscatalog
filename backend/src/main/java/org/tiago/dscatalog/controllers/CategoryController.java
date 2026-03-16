1d7d37e97b398e6f1376010050a8e6ff5a2769e3
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-03-16 18:01:48 -0300

    feat(category): add insert endpoint

--- backend/src/main/java/org/tiago/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/org/tiago/dscatalog/controllers/CategoryController.java
@@ -1,13 +1,17 @@
 package org.tiago.dscatalog.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.tiago.dscatalog.dto.CategoryDTO;
 import org.tiago.dscatalog.exceptions.EntityNotFoundException;
 import org.tiago.dscatalog.services.CategoryService;
@@ -30,5 +34,13 @@ public class CategoryController {
 		return service.findById(id)
 				.map(ResponseEntity::ok)
 				.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody CategoryDTO categoryDTO){
+		categoryDTO = service.insert(categoryDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(categoryDTO.id()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
