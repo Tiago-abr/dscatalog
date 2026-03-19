6280441ad07f679b6ad2232b9ecfcfb6ba4964d2
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-03-19 17:27:28 -0300

    refactor: rename EntityNotFoundException to ResourceNotFoundException

--- backend/src/main/java/org/tiago/dscatalog/exceptions/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.tiago.dscatalog.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-	public EntityNotFoundException(String message) {
-		super(message);
-	}
-}
