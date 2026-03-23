030d6ae1e8d2608762fb14a67e8350170684a830
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-03-23 18:00:59 -0300

    feat: add delete endpoint

--- backend/src/main/java/org/tiago/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/org/tiago/dscatalog/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,11 @@ public class CategoryController {
 	public ResponseEntity<CategoryDTO> update(@RequestBody CategoryDTO categoryDTO, @PathVariable Long id){
 		categoryDTO = service.update(id, categoryDTO);
 		return ResponseEntity.ok(categoryDTO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		this.service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
