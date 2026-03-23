1734a33966800c5ce7046381f33100218765564c
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-03-23 17:58:45 -0300

    feat: add database exception handler

--- backend/src/main/java/org/tiago/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/org/tiago/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.tiago.dscatalog.exceptions.DatabaseException;
 import org.tiago.dscatalog.exceptions.ResourceNotFoundException;
 import org.tiago.dscatalog.exceptions.StandardError;
 
@@ -23,5 +24,16 @@ public class ControllerExceptionHandler {
 		error.setMessage(exception.getMessage());
 		error.setPath(request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException exception, HttpServletRequest request){
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setError("Database exception");
+		error.setMessage(exception.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
