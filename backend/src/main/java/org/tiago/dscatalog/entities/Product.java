d12006e7c2e9473c04c019f8bb76e1f52e249cc9
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-03-27 17:08:13 -0300

    feat: add many-to-many relationship between product and category

--- backend/src/main/java/org/tiago/dscatalog/entities/Product.java
+++ backend/src/main/java/org/tiago/dscatalog/entities/Product.java
@@ -1,13 +1,18 @@
 package org.tiago.dscatalog.entities;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,7 @@ public class Product implements Serializable{
 	
 	private String name;
 	
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	
 	private Double price;
@@ -29,6 +35,11 @@ public class Product implements Serializable{
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant date;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
 	public Product() {}	
 	
@@ -88,6 +99,10 @@ public class Product implements Serializable{
 
 	public void setDate(Instant date) {
 		this.date = date;
+	}
+	
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
