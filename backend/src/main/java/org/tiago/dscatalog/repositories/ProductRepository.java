271e7be14f729ac35431b12e4c4f515c7bb1765b
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-04-08 18:00:15 -0300

    feat: Create product repository

---
+++ backend/src/main/java/org/tiago/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package org.tiago.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.tiago.dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {}
