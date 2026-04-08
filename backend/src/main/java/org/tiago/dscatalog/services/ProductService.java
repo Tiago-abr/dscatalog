4b8ca7c6f09d495a7dfa7b748145d0d185e2c71e
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-04-08 18:08:20 -0300

    feat(product): create ProductService with paginated search
    - Add ProductService class to the service layer
    - Implement findAllPaged to support pagination and sorting

---
+++ backend/src/main/java/org/tiago/dscatalog/services/ProductService.java
@@ -0,0 +1,23 @@
+package org.tiago.dscatalog.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.tiago.dscatalog.dto.ProductDTO;
+import org.tiago.dscatalog.entities.Product;
+import org.tiago.dscatalog.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
+		Page<Product> pages = repository.findAll(pageRequest);
+		return pages.map(ProductDTO::new);
+	}
+}
