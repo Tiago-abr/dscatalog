c6267b94ffab60b67b2e681ea7b237908943a8fe
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-04-13 17:05:22 -0300

    feat(product): add copyDtoToEntity service method

--- backend/src/main/java/org/tiago/dscatalog/services/ProductService.java
+++ backend/src/main/java/org/tiago/dscatalog/services/ProductService.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.tiago.dscatalog.dto.CategoryDTO;
 import org.tiago.dscatalog.dto.ProductDTO;
+import org.tiago.dscatalog.entities.Category;
 import org.tiago.dscatalog.entities.Product;
 import org.tiago.dscatalog.exceptions.ResourceNotFoundException;
+import org.tiago.dscatalog.repositories.CategoryRepository;
 import org.tiago.dscatalog.repositories.ProductRepository;
 
 @Service
@@ -15,6 +18,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
@@ -26,5 +32,19 @@ public class ProductService {
 	public ProductDTO findById(Long id) {
 		return repository.findById(id).map(product -> new ProductDTO(product, product.getCategories()))
 				.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product product) {
+		product.setName(dto.name());
+		product.setDescription(dto.description());
+		product.setPrice(dto.price());
+		product.setImgUrl(dto.imgUrl());
+		product.setDate(dto.date());
+		
+		product.getCategories().clear();
+		for(CategoryDTO categoryDto : dto.categories()) {
+			Category category = categoryRepository.getReferenceById(categoryDto.id());
+			product.getCategories().add(category);
+		}
 	}
 }
